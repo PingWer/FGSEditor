ef0ac1ea2f84a973b01ac68ab66682a759465831
PingWer <r74michele@gmail.com>
2026-04-29 00:11:13 +0200

    build fix and readme update

--- build.bat
+++ build.bat
@@ -9,18 +9,12 @@ if exist "dist" rmdir /s /q dist
 .venv\Scripts\pyinstaller --noconfirm --noconsole --onefile --icon="icon.ico" --add-data "icon.ico;." --hidden-import="fgseditor_qt.panels" --name "FGSEditor" --paths="%cd%" fgseditor_qt\fgsview_loader.py
 
 echo.
-echo Copying FGS_grain_size, FGS_size_table and Templates next to the executable...
+echo Copying FGS_grain_size and Templates next to the executable...
 if exist "dist\FGS_grain_size" rmdir /s /q dist\FGS_grain_size
 xcopy /s /e /i /q FGS_grain_size dist\FGS_grain_size
 
-if exist "dist\FGS_size_table" rmdir /s /q dist\FGS_size_table
-xcopy /s /e /i /q FGS_size_table dist\FGS_size_table
-
 if exist "dist\Templates" rmdir /s /q dist\Templates
 xcopy /s /e /i /q Templates dist\Templates
-
-echo Copying grav1synth.exe...
-if exist "grav1synth.exe" copy /y grav1synth.exe dist\grav1synth.exe
 
 echo Copying NOTICE.md...
 if exist "NOTICE.md" copy /y NOTICE.md dist\NOTICE.md
